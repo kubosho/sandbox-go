20fd56464e795ff925de6e9e4edb56fc8ffee5be
kubosho <ta2@o2p.jp>
2017-10-18 23:38:27 +0900

    feat: add CLI for fib()

--- treasure/fib.go
+++ treasure/fib.go
@@ -1,12 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func fib(n int) int {
 	if n < 2 {
 		return n
 	}
-
 	return fib(n-2) + fib(n-1)
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprint(os.Stderr, "do nothing\n")
+		os.Exit(1)
+	}
+	num, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Arguments must be integer")
+		os.Exit(1)
+	}
+	fmt.Fprint(os.Stdout, fib(num))
 }
