ea8de32bc310af256a6475718ea1ca9086eac71f
kubosho <ta2@o2p.jp>
2017-10-24 01:19:54 +0900

    feat: implemented can fetch multiple page contents

--- treasure/http-server/server.go
+++ treasure/http-server/server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/kubosho/sandbox-go/treasure/http-server/scraper"
 )
@@ -13,18 +14,53 @@ type Page struct {
 	Description string `json:"description"`
 }
 
+const separator = ","
+
+func fetchPageData(url string) (*Page, error) {
+	content, err := scraper.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Page{Title: content.Title, Description: content.Description}, nil
+}
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	url := r.FormValue("url")
 	if url == "" {
 		http.Error(w, "URL not specified", http.StatusBadRequest)
 		return
 	}
-	content, err := scraper.Get(url)
-	if err != nil {
-		panic(err)
+
+	if strings.Contains(url, separator) {
+		urls := strings.Split(url, separator)
+		var pageData []*Page
+
+		for _, u := range urls {
+			page, err := fetchPageData(u)
+			if err != nil {
+				http.Error(w, "page fetch are failed", http.StatusInternalServerError)
+				return
+			}
+
+			pageData = append(pageData, page)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		enc := json.NewEncoder(w)
+		if err := enc.Encode(pageData); err != nil {
+			http.Error(w, "encoding failed", http.StatusInternalServerError)
+			return
+		}
+		return
 	}
 
-	page := &Page{Title: content.Title, Description: content.Description}
+	page, err := fetchPageData(url)
+	if err != nil {
+		http.Error(w, "page fetch are failed", http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 
